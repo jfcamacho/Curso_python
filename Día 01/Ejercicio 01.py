b6f738a29e199f1b252797e8faf96685c8c34730
Jefferson <jefferson-camacho@hotmail.com>
2022-10-11 13:41:08 -0500

    Día 01

---
+++ Día 01/Ejercicio 01.py
@@ -0,0 +1,20 @@
+# ************************ INTRODUCCIÓN A PYTHON ****************************
+
+# -- Instalación de python
+
+        # -- Descarga de Python.:  https://www.python.org/downloads/
+        # -- Descarga de visual studio code.: https://code.visualstudio.com/download
+
+# -- Editores de programación
+
+# -- Expresiones
+
+# -- Declaraciones
+
+# -- Variables
+
+# -- Tipos e ingreso de datos
+
+# -- Manejo de cadenas de caractéres
+
+# -- Taller 1
